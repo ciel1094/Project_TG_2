cf7c0e2064d5bf45578bc4913566bac10e08cc10
unknown <kjieon0626@gmail.com>
2021-10-29 20:13:30 +0900

    db fix

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const mysql2 = require("mysql2");
 
-const db = mysql.createPool({
+const db = mysql2.createPool({
     host: "127.0.0.1",
     port: "3306",
-    database: "",
+    database: "TG_db",
     user: "root",
     password: "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"
 });
